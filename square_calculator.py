12df28ffca95994175be37eb0846cd47ed35c961
Junaid-Qadir-UniGe <jndqadar@gmail.com>
2023-12-08 23:27:42 +0100

    Remove files or directory

--- square_calculator.py
+++
@@ -1,9 +0,0 @@
-# square_calculator.py
-
-def square(number):
-    return number ** 2
-
-if __name__ == "__main__":
-    user_input = float(input("Enter a number: "))
-    result = square(user_input)
-    print(f"The square of {user_input} is {result}")
